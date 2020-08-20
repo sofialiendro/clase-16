0e241bca4a4e12f81e5e1c7b783c83f363dbb9e3
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-20 20:55:08 -0300

    Agrego ejercicios

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Ejercicio 1
+
 // const verificarEdadDelUsuario = prompt("Decime tu edad.")
 
 // if (edadDelUsuario >=18) {
@@ -8,6 +10,11 @@
 //     alert("No podés entrar todavía.")
 // }
 
+
+
+
+
+// Ejercicio 2
 
 // const edadDelUsuarioUno = 18
 
@@ -24,22 +31,92 @@
 // console.log(chequearEdadDelUsuario(edadDelUsuarioUno))
 // console.log(chequearEdadDelUsuario(12))
 
-const avanzarSemaforo = (colorActual) => {
-    if (colorActual == "verde") {
-        return "amarillo"
-    }
 
-    else if (colorActual == "amarillo") {
-        return "rojo"
-    }
 
-    else if (colorActual == "rojo") {
-        return "verde"
-    }
+
+
+// Ejercicio 3
+
+// const avanzarSemaforo = (colorActual) => {
+//     if (colorActual == "verde") {
+//         return "amarillo"
+//     }
+
+//     else if (colorActual == "amarillo") {
+//         return "rojo"
+//     }
+
+//     else if (colorActual == "rojo") {
+//         return "verde"
+//     }
     
+// }
+
+// console.log(avanzarSemaforo("verde"))     
+// console.log(avanzarSemaforo("amarillo"))
+// console.log(avanzarSemaforo("rojo")) 
+
+
+
+
+
+// Ejercicio 4
+
+// const puedeVerPelicula = (edad, tieneAutorizacion) => {
+//     if (edad >= 15 || tieneAutorizacion === true) {
+//         return true
+//     }
+
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(puedeVerPelicula(12, false)) 
+// console.log(puedeVerPelicula(12, true))  
+// console.log(puedeVerPelicula(16, false)) 
+// console.log(puedeVerPelicula(18, true))
+
+
+
+
+
+// Ejercicio 5
+
+// const esConsonante = (letra) => {
+//     if (letra !== "a" && letra !== "e" && letra !== "i" && letra !== "o" && letra !== "u") {
+//         return true
+//     }
+
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(esConsonante("a")) 
+// console.log(esConsonante("n")) 
+// console.log(esConsonante("e"))
+
+
+
+
+
+// Ejercicio 6
+
+const puedeRenovarCarnet = (pasoTests, tieneMultasImpagas, pagoImpuestos) => {
+    if (pasoTests && tieneMultasImpagas === false && pagoImpuestos) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
-console.log(avanzarSemaforo("verde"))     
-console.log(avanzarSemaforo("amarillo"))
-console.log(avanzarSemaforo("rojo")) 
-
+console.log(puedeRenovarCarnet(true, true, true))    
+console.log(puedeRenovarCarnet(true, true, false))   
+console.log(puedeRenovarCarnet(true, false, true))   
+console.log(puedeRenovarCarnet(true, false, false))  
+console.log(puedeRenovarCarnet(false, true, true))   
+console.log(puedeRenovarCarnet(false, true, false))  
+console.log(puedeRenovarCarnet(false, false, true))  
+console.log(puedeRenovarCarnet(false, false, false)) 
